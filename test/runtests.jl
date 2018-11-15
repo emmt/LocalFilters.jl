96571cc2482133d3ba8cab4bd89a29a0e463a5fa
Éric Thiébaut <eric.thiebaut@univ-lyon1.fr>
2018-11-15 10:20:40 +0100

    Test ball(), strictfloor(), _range(), ...

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,8 +5,8 @@ module LocalFiltersTests
 using Compat
 using LocalFilters, Compat.Test
 using LocalFilters: Neighborhood, RectangularBox, Kernel,
-    axes, initialindex, finalindex, limits, cartesianregion,
-    USE_CARTESIAN_RANGE
+    axes, initialindex, finalindex, limits, cartesianregion, ball,
+    strictfloor, USE_CARTESIAN_RANGE, _range
 
 replicate(a, n::Integer) = ntuple(i->a, n)
 compare(a, b) = maximum(abs(a - b))
@@ -22,103 +22,124 @@ const triviallocalextrema = localextrema
         for T in (Int, Float32)
             @test limits(T) === (typemin(T), typemax(T))
         end
+        for dim in (11, 12)
+            @test _range(Int8(dim)) === _range(dim)
+        end
+        @test_throws ArgumentError _range(0)
+        @test strictfloor(Int, 5*(1 + eps(Float64))) == 5
+        @test strictfloor(Int, 5.0) == 4
     end
 
     @testset "Neighborhoods" begin
-        for (B1, B2) in (((3,), (-1:1,)),
-                         ((3, 4, 5), (-1:1, -2:1, -2:2)))
-            N = length(B1)
-            box = Neighborhood(B1)
+        for (dims, rngs) in (((3,), (-1:1,)),
+                             ((3, 4, 5), (-1:1, -2:1, -2:2)))
+            N = length(dims)
+            box = Neighborhood(dims)
             I1, I2 = limits(box)
-            inds = map((i,j) -> i:j, I1.I, I2.I)
-            A = Array{Int}(undef, size(box))
+            A = Array{Int}(undef, dims)
 
             # Test limits(), initialindex() and finalindex().
-            @test initialindex(CartesianIndices(inds)) === I1
-            @test finalindex(CartesianIndices(inds)) === I2
-            @test limits(CartesianIndices(inds)) === (I1, I2)
+            @test initialindex(CartesianIndices(rngs)) === I1
+            @test finalindex(CartesianIndices(rngs)) === I2
+            @test limits(CartesianIndices(rngs)) === (I1, I2)
             @static if USE_CARTESIAN_RANGE
-                @test initialindex(CartesianRange(inds)) === I1
-                @test finalindex(CartesianRange(inds)) === I2
-                @test limits(CartesianRange(inds)) === (I1, I2)
+                @test initialindex(CartesianRange(rngs)) === I1
+                @test finalindex(CartesianRange(rngs)) === I2
+                @test limits(CartesianRange(rngs)) === (I1, I2)
             end
             @test initialindex(box) === I1
             @test finalindex(box) === I2
             @test limits(box) === (I1, I2)
+            @test initialindex(A) === one(CartesianIndex{N})
+            @test finalindex(A) === CartesianIndex(size(A))
+            @test limits(A) === (one(CartesianIndex{N}),
+                                 CartesianIndex(size(A)))
 
             # Test cartesianregion().
             @static if USE_CARTESIAN_RANGE
-                region = CartesianRange(inds)
+                region = CartesianRange(rngs)
                 @test cartesianregion(I1,I2) === region
-                @test cartesianregion(CartesianRange(inds)) === region
-                @test cartesianregion(CartesianIndices(inds)) === region
+                @test cartesianregion(CartesianRange(rngs)) === region
+                @test cartesianregion(CartesianIndices(rngs)) === region
                 @test cartesianregion(box) === region
                 @test cartesianregion(A) === CartesianRange(size(A))
             else
-                region = CartesianIndices(inds)
+                region = CartesianIndices(rngs)
                 @test cartesianregion(I1,I2) === region
-                @test cartesianregion(CartesianIndices(inds)) === region
+                @test cartesianregion(CartesianIndices(rngs)) === region
                 @test cartesianregion(box) === region
                 @test cartesianregion(A) === CartesianIndices(A)
             end
 
             # Neighborhood constructors.
-            @test Neighborhood(B1...) === box
-            @test Neighborhood(B2) === box
-            @test Neighborhood(B2...) === box
-            @test Neighborhood(CartesianIndices(inds)) === box
+            @test Neighborhood(box) === box
+            @test Neighborhood(dims...) === box
+            @test Neighborhood(rngs) === box
+            @test Neighborhood(rngs...) === box
+            @test Neighborhood(CartesianIndices(rngs)) === box
             @static if USE_CARTESIAN_RANGE
-                @test Neighborhood(CartesianRange(inds)) === box
+                @test Neighborhood(CartesianRange(rngs)) === box
                 @test Neighborhood(CartesianRange(I1, I2)) === box
             end
+            @test convert(Neighborhood, rngs) === box
+            @test convert(Neighborhood, dims) === box
 
             # RectangularBox constructors.
-            @test RectangularBox(B1) === box
-            @test RectangularBox(B1...) === box
-            @test RectangularBox(B2) === box
-            @test RectangularBox(B2...) === box
+            @test RectangularBox(box) === box
+            @test RectangularBox(dims) === box
+            @test RectangularBox(dims...) === box
+            @test RectangularBox(rngs) === box
+            @test RectangularBox(rngs...) === box
             @static if USE_CARTESIAN_RANGE
-                @test CartesianRange(box) === CartesianRange(inds)
+                @test CartesianRange(box) === CartesianRange(rngs)
                 @test CartesianRange(box) === CartesianRange(I1, I2)
             end
+            @test convert(RectangularBox, rngs) === box
+            @test convert(RectangularBox, dims) === box
+            dim = dims[end]
+            rng = rngs[end]
+            @test RectangularBox{N}(dim) === RectangularBox(ntuple(d -> dim, N))
+            @test RectangularBox{N}(rng) === RectangularBox(ntuple(d -> rng, N))
 
-            # Conversion RectangularBox <-> CartesianIndices.
-            @test CartesianIndices(box) === CartesianIndices(inds)
-            @test RectangularBox(CartesianIndices(inds)) === box
-            @test convert(RectangularBox, CartesianIndices(inds)) === box
-            @test convert(RectangularBox{N}, CartesianIndices(inds)) === box
-            @test CartesianIndices(box) === CartesianIndices(inds)
-            @test convert(CartesianIndices, box) === CartesianIndices(inds)
-            @test convert(CartesianIndices{N}, box) === CartesianIndices(inds)
+            # Conversions RectangularBox <-> CartesianIndices.
+            @test CartesianIndices(box) === CartesianIndices(rngs)
+            @test RectangularBox(CartesianIndices(rngs)) === box
+            @test convert(RectangularBox, CartesianIndices(rngs)) === box
+            @test convert(RectangularBox{N}, CartesianIndices(rngs)) === box
+            @test CartesianIndices(box) === CartesianIndices(rngs)
+            @test convert(CartesianIndices, box) === CartesianIndices(rngs)
+            @test convert(CartesianIndices{N}, box) === CartesianIndices(rngs)
             @static if USE_CARTESIAN_RANGE
                 # Conversion RectangularBox <-> CartesianRange.
-                @test RectangularBox(CartesianRange(inds)) === box
-                @test convert(RectangularBox, CartesianRange(inds)) === box
-                @test convert(RectangularBox{N}, CartesianRange(inds)) === box
-                @test CartesianRange(box) === CartesianRange(inds)
-                @test convert(CartesianRange, box) === CartesianRange(inds)
-                @test convert(CartesianRange{CartesianIndex{N}}, box) === CartesianRange(inds)
+                @test RectangularBox(CartesianRange(rngs)) === box
+                @test convert(RectangularBox, CartesianRange(rngs)) === box
+                @test convert(RectangularBox{N}, CartesianRange(rngs)) === box
+                @test CartesianRange(box) === CartesianRange(rngs)
+                @test convert(CartesianRange, box) === CartesianRange(rngs)
+                @test convert(CartesianRange{CartesianIndex{N}}, box) === CartesianRange(rngs)
             end
 
             # Conversion Neighborhood <-> CartesianIndices.
-            @test Neighborhood(CartesianIndices(inds)) === box
-            @test convert(Neighborhood, CartesianIndices(inds)) === box
-            @test convert(Neighborhood{N}, CartesianIndices(inds)) === box
-            @test CartesianIndices(box) === CartesianIndices(inds)
-            @test convert(CartesianIndices, box) === CartesianIndices(inds)
-            @test convert(CartesianIndices{N}, box) === CartesianIndices(inds)
+            @test Neighborhood(CartesianIndices(rngs)) === box
+            @test convert(Neighborhood, CartesianIndices(rngs)) === box
+            @test convert(Neighborhood{N}, CartesianIndices(rngs)) === box
+            @test CartesianIndices(box) === CartesianIndices(rngs)
+            @test convert(CartesianIndices, box) === CartesianIndices(rngs)
+            @test convert(CartesianIndices{N}, box) === CartesianIndices(rngs)
             @static if USE_CARTESIAN_RANGE
                 # Conversion Neighborhood <-> CartesianRange
-                @test Neighborhood(CartesianRange(inds)) === box
-                @test convert(Neighborhood, CartesianRange(inds)) === box
-                @test convert(Neighborhood{N}, CartesianRange(inds)) === box
-                @test CartesianRange(box) === CartesianRange(inds)
-                @test convert(CartesianRange, box) === CartesianRange(inds)
-                @test convert(CartesianRange{CartesianIndex{N}}, box) === CartesianRange(inds)
+                @test Neighborhood(CartesianRange(rngs)) === box
+                @test convert(Neighborhood, CartesianRange(rngs)) === box
+                @test convert(Neighborhood{N}, CartesianRange(rngs)) === box
+                @test CartesianRange(box) === CartesianRange(rngs)
+                @test convert(CartesianRange, box) === CartesianRange(rngs)
+                @test convert(CartesianRange{CartesianIndex{N}}, box) ===
+                    CartesianRange(rngs)
             end
 
-            @test length(box) === length(CartesianIndices(inds))
-            @test size(box) === size(CartesianIndices(inds))
+            # Other basic methods.
+            @test length(box) === length(CartesianIndices(rngs))
+            @test size(box) === size(CartesianIndices(rngs))
             @test size(box) === ntuple(d -> size(box, d), N)
             @test axes(box) === ntuple(d -> axes(box, d), N)
         end
@@ -159,12 +180,26 @@ const triviallocalextrema = localextrema
         end
     end
 
+    ball3x3 = Bool[1 1 1;
+                   1 1 1;
+                   1 1 1];
+    ball5x5 = Bool[0 1 1 1 0;
+                   1 1 1 1 1;
+                   1 1 1 1 1;
+                   1 1 1 1 1;
+                   0 1 1 1 0];
+    ball7x7 = Bool[0 0 1 1 1 0 0;
+                   0 1 1 1 1 1 0;
+                   1 1 1 1 1 1 1;
+                   1 1 1 1 1 1 1;
+                   1 1 1 1 1 1 1;
+                   0 1 1 1 1 1 0;
+                   0 0 1 1 1 0 0];
+    @test samevalues(ball3x3, ball(2, 1))
+    @test samevalues(ball5x5, ball(2, 2))
+    @test samevalues(ball7x7, ball(2, 3))
+
     @testset "2D test with a 5x5 ball" begin
-        ball5x5 = Bool[0 1 1 1 0;
-                       1 1 1 1 1;
-                       1 1 1 1 1;
-                       1 1 1 1 1;
-                       0 1 1 1 0];
         a = rand(162,181)
         mask = Kernel(ball5x5)
         kern = Kernel(eltype(a), mask)
