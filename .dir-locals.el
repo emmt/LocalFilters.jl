88114a79fed6160ccf2d4e34643fb1307f26e854
Éric Thiébaut <eric.thiebaut@univ-lyon1.fr>
2024-11-15 17:23:25 +0100

    Change fill column

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,7 +1,7 @@
 ((nil . ((indent-tabs-mode . nil)
 	 (tab-width . 8)
          ;;(coding . utf8)
-	 (fill-column . 79)
+	 (fill-column . 90)
 	 (ispell-local-dictionary . "american")))
  (c-mode . ((c-file-style . "bsd")
 	    (c-basic-offset . 2)))
