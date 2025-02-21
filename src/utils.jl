a24975b745f8a5801c1b08ce64a69f658ed2e980
Éric Thiébaut <eric.thiebaut@univ-lyon1.fr>
2025-02-21 08:34:23 +0100

    Update `Indices` doc.

--- src/utils.jl
+++ src/utils.jl
@@ -12,24 +12,23 @@
 #
 
 """
-    LocalFilters.Indices(A...) -> indices
+    I = LocalFilters.Indices(A::AbstractArray...)
 
-yields a callable object that can be used to produce ranges of indices for each of the
-arrays `A...`. These ranges will all be of the same type: linear index ranges, if all
-arrays `A...` are vectors implementing fast linear indexing, Cartesian index ranges
-otherwise.
+builds a callable object, `I`, that can be used to produce ranges of indices for each of
+the arrays `A...`. These ranges will all be of the same type: linear index ranges, if all
+arrays `A...` are vectors, Cartesian index ranges otherwise.
 
-The returned object is similar to the `eachindex` method but specialized for a style of
-indexing, it can be used as `indices(B...)` to yield a suitable index range to access all
-the entries of array(s) `B...` which are any number of the `A...` specified when building
-the `indices` object. If `B...` consists in several arrays, they must have the same axes.
+`I` is similar to the `eachindex` method but is specialized for a style of indexing, it
+can be called as `I(B...)` to yield a suitable index range to access all the entries of
+array(s) `B...` which are any number of the `A...` specified when building `I`. If `B...`
+consists in several arrays, they must have the same axes otherwise `I(B...)` will throw a
+`DimensionMismatch` exception.
 
 Call:
 
-    LocalFilters.Indices{S}()
+   I = LocalFilters.Indices{S}()
 
-with `S = IndexLinear` or `S = IndexCartesian` to specifically choose the
-indexing style.
+with `S = IndexLinear` or `S = IndexCartesian` to specifically choose the indexing style.
 
 """ Indices
 @public Indices
