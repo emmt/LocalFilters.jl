8137d7e734b097e236ed23f5713fb3133110da54
Éric Thiébaut <eric.thiebaut@univ-lyon1.fr>
2025-02-26 18:32:49 +0100

    More tests for `reverse` and `reverse!`

--- test/runtests.jl
+++ test/runtests.jl
@@ -321,79 +321,96 @@ f2(x) = x > 0.5
             @test (0x1,0x1,0x1) === @inferred ntuple(Returns{UInt8}(1), Val(3))
         end
 
-        @testset "`reverse` and `reverse!` with `dims=$d`" for d in (Colon(), 1, 2)
-            if d isa Colon || d == 1
-                # Check for vectors.
-                A = 1:12
-                R = 12:-1:1
-                B = @inferred reverse(A; dims=d)
-                @test B == R
-                @test B isa AbstractRange{eltype(A)}
-                @test step(B) == -step(A)
-                @test last(B) == first(A)
-                @test first(B) == last(A)
-                C = Array{eltype(A)}(undef, size(A))
-                @test C === @inferred reverse!(copyto!(C, A); dims=d)
-                @test C == R
-                if d isa Colon
-                    @test R == @inferred reverse(A)
-                    @test C === @inferred reverse!(copyto!(C, A))
-                    @test C == R
-                end
-                A = -2:3:7
-                R = 7:-3:-2
-                B = @inferred reverse(A; dims=d)
-                @test B == R
-                @test B isa AbstractRange{eltype(A)}
-                @test step(B) == -step(A)
-                @test last(B) == first(A)
-                @test first(B) == last(A)
-                C = Array{eltype(A)}(undef, size(A))
-                @test C === @inferred reverse!(copyto!(C, A); dims=d)
-                @test C == R
-                if d isa Colon
-                    @test R == @inferred reverse(A)
-                    @test C === @inferred reverse!(copyto!(C, A))
-                    @test C == R
-                end
+        @testset "`reverse` and `reverse!`" begin
+            @testset "... basic use" begin
+                @test reverse("abcd") == "dcba"
+                r = 1:8
+                v = Array{Int}(undef, length(r))
+                @test reverse(r, 2) == [1,8,7,6,5,4,3,2]
+                @test reverse(r, 3, 6) == [1,2,6,5,4,3,7,8]
+                @test reverse(r; dims=:) === 8:-1:1
+                @test reverse(r; dims=1) === 8:-1:1
+                @test reverse(copyto!(v, r); dims=:) == 8:-1:1
+                @test_throws ArgumentError reverse(r; dims=0)
+                @test v === reverse!(copyto!(v, r), 2) && v == [1,8,7,6,5,4,3,2]
+                @test v === reverse!(copyto!(v, r), 3, 6) && v == [1,2,6,5,4,3,7,8]
+                @test v === reverse!(copyto!(v, r); dims=:) && v == [8,7,6,5,4,3,2,1]
+                @test_throws ArgumentError reverse!(v; dims=0)
             end
-            if d isa Colon || d ≤ 2
-                # Check for 2-dimensional arrays.
-                A = reshape(1:12, 3,4)
-                R = d isa Colon ? reshape(12:-1:1, size(A)) : Base.reverse(A; dims=d)
-                C = Array{eltype(A)}(undef, size(A))
-                @test R == @inferred reverse(A; dims=d)
-                @test C === @inferred reverse!(copyto!(C, A); dims=d)
-                @test C == R
-                if d isa Colon
-                    @test R == @inferred reverse(A)
-                    @test C === @inferred reverse!(copyto!(C, A))
+            @testset "... with `dims=$d`" for d in (Colon(), 1, 2)
+                if d isa Colon || d == 1
+                    # Check for vectors.
+                    A = 1:12
+                    R = 12:-1:1
+                    B = @inferred reverse(A; dims=d)
+                    @test B == R
+                    @test B isa AbstractRange{eltype(A)}
+                    @test step(B) == -step(A)
+                    @test last(B) == first(A)
+                    @test first(B) == last(A)
+                    C = Array{eltype(A)}(undef, size(A))
+                    @test C === @inferred reverse!(copyto!(C, A); dims=d)
                     @test C == R
+                    if d isa Colon
+                        @test R == @inferred reverse(A)
+                        @test C === @inferred reverse!(copyto!(C, A))
+                        @test C == R
+                    end
+                    A = -2:3:7
+                    R = 7:-3:-2
+                    B = @inferred reverse(A; dims=d)
+                    @test B == R
+                    @test B isa AbstractRange{eltype(A)}
+                    @test step(B) == -step(A)
+                    @test last(B) == first(A)
+                    @test first(B) == last(A)
+                    C = Array{eltype(A)}(undef, size(A))
+                    @test C === @inferred reverse!(copyto!(C, A); dims=d)
+                    @test C == R
+                    if d isa Colon
+                        @test R == @inferred reverse(A)
+                        @test C === @inferred reverse!(copyto!(C, A))
+                        @test C == R
+                    end
                 end
-                A = reshape(1:12, 4,3)
-                C = Array{eltype(A)}(undef, size(A))
-                R = d isa Colon ? reshape(12:-1:1, size(A)) : Base.reverse(A; dims=d)
-                @test R == @inferred reverse(A; dims=d)
-                @test C === @inferred reverse!(copyto!(C, A); dims=d)
-                @test C == R
-                if d isa Colon
-                    @test R == @inferred reverse(A)
-                    @test C === @inferred reverse!(copyto!(C, A))
+                if d isa Colon || d ≤ 2
+                    # Check for 2-dimensional arrays.
+                    A = reshape(1:12, 3,4)
+                    R = d isa Colon ? reshape(12:-1:1, size(A)) : Base.reverse(A; dims=d)
+                    C = Array{eltype(A)}(undef, size(A))
+                    @test R == @inferred reverse(A; dims=d)
+                    @test C === @inferred reverse!(copyto!(C, A); dims=d)
                     @test C == R
+                    if d isa Colon
+                        @test R == @inferred reverse(A)
+                        @test C === @inferred reverse!(copyto!(C, A))
+                        @test C == R
+                    end
+                    A = reshape(1:12, 4,3)
+                    C = Array{eltype(A)}(undef, size(A))
+                    R = d isa Colon ? reshape(12:-1:1, size(A)) : Base.reverse(A; dims=d)
+                    @test R == @inferred reverse(A; dims=d)
+                    @test C === @inferred reverse!(copyto!(C, A); dims=d)
+                    @test C == R
+                    if d isa Colon
+                        @test R == @inferred reverse(A)
+                        @test C === @inferred reverse!(copyto!(C, A))
+                        @test C == R
+                    end
                 end
-            end
-            if d isa Colon || d ≤ 3
-                # Check for 3-dimensional arrays.
-                A = reshape(-4:5:111, 2,3,4)
-                R = d isa Colon ? reshape(111:-5:-4, size(A)) : Base.reverse(A; dims=d)
-                C = Array{eltype(A)}(undef, size(A))
-                @test R == @inferred reverse(A; dims=d)
-                @test C === @inferred reverse!(copyto!(C, A); dims=d)
-                @test C == R
-                if d isa Colon
-                    @test R == @inferred reverse(A)
-                    @test C === @inferred reverse!(copyto!(C, A))
+                if d isa Colon || d ≤ 3
+                    # Check for 3-dimensional arrays.
+                    A = reshape(-4:5:111, 2,3,4)
+                    R = d isa Colon ? reshape(111:-5:-4, size(A)) : Base.reverse(A; dims=d)
+                    C = Array{eltype(A)}(undef, size(A))
+                    @test R == @inferred reverse(A; dims=d)
+                    @test C === @inferred reverse!(copyto!(C, A); dims=d)
                     @test C == R
+                    if d isa Colon
+                        @test R == @inferred reverse(A)
+                        @test C === @inferred reverse!(copyto!(C, A))
+                        @test C == R
+                    end
                 end
             end
         end
